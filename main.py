0953335f2b90550ab1457e33aab35a849cdd428f
iamsparkedev <sifathasansami2@gmail.com>
2025-09-23 16:33:48 +0600

    Update Main.py and add logics

--- main.py
+++ main.py
@@ -1,44 +1,50 @@
 import customtkinter as ctk
 from tkinter import messagebox
 import os
-
+import webbrowser
 # Use the imported alias 'ctk' when creating the CTk application instance
 
+#Github btn Logic
+def GithubButton():
+    webbrowser.open("https://github.com/iamsparkedev/Flows")
 
 
+#About me btn logic
+def AboutMeButton():
+    webbrowser.open("https://github.com/iamsparkedev/")
+
+
+#Main app window
 app = ctk.CTk()
 app.geometry("600x400")
 # app.title("Flows - Easy to use video downloader")
-app._set_appearance_mode("dark")
+# app._set_appearance_mode("dark")
 app.resizable(False, False)
-
-
-
-
+# ctk.set_default_color_theme("dark-blue")
 
 # Left frame for navigation
-left_frame = ctk.CTkFrame(app, width=150, height=400, corner_radius=0)
+left_frame = ctk.CTkFrame(app, width=150, height=400, corner_radius=0, fg_color="transparent")
 left_frame.place(x=0, y=0)
 
 # about me button
-nav_btn1 = ctk.CTkButton(left_frame, text="About me", width=120, height=32, corner_radius=8)
+nav_btn1 = ctk.CTkButton(left_frame, text="About me", width=120, height=32, corner_radius=8, command=AboutMeButton)
 nav_btn1.place(x=15, y=30)
-#source code button
-nav_btn1 = ctk.CTkButton(left_frame, text="GitHub", width=120, height=32, corner_radius=8)
+
+#github button
+nav_btn1 = ctk.CTkButton(left_frame, text="GitHub", width=120, height=32, corner_radius=8, command=GithubButton)
 nav_btn1.place(x=15, y=72)
 
 # Right frame for main content
-right_frame = ctk.CTkFrame(app, width=450, height=400, corner_radius=0)
+right_frame = ctk.CTkFrame(app, width=450, height=400, corner_radius=0, fg_color="transparent")
 right_frame.place(x=150, y=0)
 #url rntry
 entry = ctk.CTkEntry(right_frame, placeholder_text="Enter URL", width=200, height=32, corner_radius=8)
 
 #button
-button = ctk.CTkButton(right_frame, text="Download", width=80, height=32, corner_radius=8)
-button.place(x=300, y=160)
-#label
-label = ctk.CTkLabel(right_frame, text="Flows", font=ctk.CTkFont(size=20, weight="bold"))
+button = ctk.CTkButton(right_frame, text="Download", width=80, height=32, corner_radius=8,fg_color="#5DD9D9" ,hover_color="#A0F2F2", border_color="#464A4A", border_width=2, font=ctk.CTkFont(size=10, weight="bold"))
 
+#labels
+label = ctk.CTkLabel(right_frame, text="Flows", font=ctk.CTkFont(size=20, weight="bold"))
 label2 = ctk.CTkLabel(right_frame, text="Enter Video URL :-", font=ctk.CTkFont(size=14))
 
 # Move your widgets into right_frame instead of app
@@ -53,7 +59,7 @@ label2.place(in_=right_frame, x=100, y=90)
 
 # Style updates
 app.title("⚡ Flows - Easy to use video downloader")
-label.configure(text="⚡ Flows", font=ctk.CTkFont(size=24, weight="bold"), text_color="#00FFFF")
-label2.configure(font=ctk.CTkFont(size=14, weight="bold"), text_color="#AAAAFF")
+label.configure(text="⚡ Flows", font=ctk.CTkFont(size=24, weight="bold"), text_color="#5DD9D9")
+label2.configure(font=ctk.CTkFont(size=10, weight="bold"), text_color="#AAAAFF")
 
 app.mainloop()
